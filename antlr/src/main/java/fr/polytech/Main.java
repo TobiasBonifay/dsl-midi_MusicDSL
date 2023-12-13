63a1e77d1f25ffb3d8466d52377cabd3fd5453aa
TobiasBonifay <tobias.bonifay@etu.univ-cotedazur.fr>
2023-12-13 21:09:52 +0100

    Partial commit of kernel MVP
    #3 Default value -> IN PROGRESS
    #5 Handle flat and sharp -> DONE
    #6 Link instruments to MIDI -> DONE
    #9 BPM -> IN PROGRESS / Note duration -> DONE
    #12 Add basic instruments -> DONE
    #13 export of .midi -> DONE

--- antlr/src/main/java/fr/polytech/Main.java
+++ antlr/src/main/java/fr/polytech/Main.java
@@ -1,32 +1,64 @@
 package fr.polytech;
 
-import fr.polytech.grammar.MusicDSLLexer;
-import fr.polytech.grammar.MusicDSLParser;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 
+import javax.sound.midi.InvalidMidiDataException;
 import java.io.IOException;
 import java.nio.file.Paths;
 
 /**
- * The program starting point. Should be run with the path to a MusicDSL file as argument.
+ * The program's entry point for converting MusicDSL files to MIDI format.
  */
 public class Main {
+    private static final String OUTPUT_FILENAME = "output.midi";
+
+    public static void main(String[] args) {
+        try {
+            if (args.length != 1) {
+                System.err.println("Usage: java Main <path_to_MusicDSL_file>");
+                return;
+            }
+            processMusicFile(args[0]);
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+        } catch (InvalidMidiDataException e) {
+            System.err.println("Error processing MIDI data: " + e.getMessage());
+        }
+    }
+
     /**
-     * Read a MusicDSL file and print the parse tree.
+     * Processes a MusicDSL file.
      *
-     * @param args the path to the MusicDSL file
-     * @throws IOException if the file cannot be read
+     * @param filePath The path to the file.
+     * @throws IOException              When an I/O error occurs.
+     * @throws InvalidMidiDataException When the MIDI data is invalid.
      */
-    public static void main(String[] args) throws IOException {
-        String inputFile = args[0];
-        CharStream input = CharStreams.fromPath(Paths.get(inputFile));
-        MusicDSLLexer lexer = new MusicDSLLexer(input);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        MusicDSLParser parser = new MusicDSLParser(tokens);
-        ParseTree tree = parser.musicComposition();
-        System.out.println(tree.toStringTree(parser));
+    private static void processMusicFile(String filePath) throws IOException, InvalidMidiDataException {
+        final CharStream input = CharStreams.fromPath(Paths.get(filePath));
+        final MusicDSLLexer lexer = new MusicDSLLexer(input);
+        final CommonTokenStream tokens = new CommonTokenStream(lexer);
+        final MusicDSLParser parser = new MusicDSLParser(tokens);
+        final ParseTree tree = parser.musicComposition();
+
+        // System.out.println(tree.toStringTree(parser));
+
+        generateMIDI(tree);
+    }
+
+    /**
+     * Generates a MIDI file from a parse tree.
+     *
+     * @param tree The parse tree.
+     * @throws InvalidMidiDataException When the MIDI data is invalid.
+     * @throws IOException              When an I/O error occurs.
+     */
+    private static void generateMIDI(ParseTree tree) throws InvalidMidiDataException, IOException {
+        MidiGeneratorVisitor visitor = new MidiGeneratorVisitor();
+        visitor.visit(tree);
+        visitor.writeMidiFile(OUTPUT_FILENAME);
+        System.out.println("MIDI file generated: " + OUTPUT_FILENAME);
     }
 }
