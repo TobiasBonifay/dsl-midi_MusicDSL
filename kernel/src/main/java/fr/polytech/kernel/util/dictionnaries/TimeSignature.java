6bcf0dc6469be33e8385eae3668bef38634878e5
TobiasBonifay <tobias.bonifay@etu.univ-cotedazur.fr>
2023-12-18 07:48:33 +0100

    #10 tostring of TimeSignature object
    Signed-off-by: TobiasBonifay <tobias.bonifay@etu.univ-cotedazur.fr>

--- kernel/src/main/java/fr/polytech/kernel/util/dictionnaries/TimeSignature.java
+++ kernel/src/main/java/fr/polytech/kernel/util/dictionnaries/TimeSignature.java
@@ -5,4 +5,9 @@ public record TimeSignature(int numerator, int denominator) {
         // MIDI time signature is represented by four bytes: numerator, denominator (as a power of 2), MIDI clocks per metronome click, and number of notated 32nd notes in a MIDI quarter note (24 and 8 are typical values)
         return new byte[]{(byte) numerator, (byte) (Math.log(denominator) / Math.log(2)), 24, 8};
     }
+
+    @Override
+    public String toString() {
+        return numerator + "/" + denominator;
+    }
 }
