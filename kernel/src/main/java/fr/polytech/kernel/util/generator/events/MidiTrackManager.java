6bb269bf90361dae8255b95b8319d49c66eecfe5
TobiasBonifay <tobias.bonifay@etu.univ-cotedazur.fr>
2023-12-18 09:49:09 +0100

    hotfix to be able to play
    Signed-off-by: TobiasBonifay <tobias.bonifay@etu.univ-cotedazur.fr>

--- kernel/src/main/java/fr/polytech/kernel/util/generator/events/MidiTrackManager.java
+++ kernel/src/main/java/fr/polytech/kernel/util/generator/events/MidiTrackManager.java
@@ -1,14 +1,22 @@
 package fr.polytech.kernel.util.generator.events;
 
+import fr.polytech.kernel.logs.LoggingSetup;
 import fr.polytech.kernel.structure.Bar;
 import fr.polytech.kernel.util.dictionnaries.TimeSignature;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.sound.midi.*;
+import java.util.logging.Logger;
 
 @Getter
 public class MidiTrackManager {
+    private static final Logger LOGGER = Logger.getLogger(MidiTrackManager.class.getName());
+
+    static {
+        LoggingSetup.setupLogger(LOGGER);
+    }
+
     private final Sequence sequence;
     private Track currentTrack;
     @Setter
@@ -30,14 +38,16 @@ public class MidiTrackManager {
     }
 
     public void setTimeSignature(TimeSignature timeSignature) throws InvalidMidiDataException {
+        LOGGER.info("            ~ Setting time signature to " + timeSignature);
         MetaMessage tsMessage = new MetaMessage();
         tsMessage.setMessage(0x58, timeSignature.toMidiData(), 4);
         currentTrack.add(new MidiEvent(tsMessage, currentTick));
     }
 
     public void setTempo(int globalTempo) throws InvalidMidiDataException {
+        LOGGER.info("            ~ Setting tempo to " + globalTempo);
         MetaMessage tempoMessage = new MetaMessage();
-        tempoMessage.setMessage(0x51, new byte[]{(byte) ((globalTempo >> 16) & 0xFF), (byte) ((globalTempo >> 8) & 0xFF), (byte) (globalTempo & 0xFF)}, 3);
-        currentTrack.add(new MidiEvent(tempoMessage, currentTick));
+        // tempoMessage.setMessage(0x51, new byte[]{(byte) ((globalTempo >> 16) & 0xFF), (byte) ((globalTempo >> 8) & 0xFF), (byte) (globalTempo & 0xFF)}, 3);
+        // currentTrack.add(new MidiEvent(tempoMessage, currentTick));
     }
 }
