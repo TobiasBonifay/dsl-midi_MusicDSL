2a9a75a5e253746c840af420c053942bcb3ddb93
TobiasBonifay <tobias.bonifay@etu.univ-cotedazur.fr>
2024-01-08 11:24:43 +0100

    getter hotfix
    Signed-off-by: TobiasBonifay <tobias.bonifay@etu.univ-cotedazur.fr>

--- kernel/src/main/java/fr/polytech/kernel/structure/Instrument.java
+++ kernel/src/main/java/fr/polytech/kernel/structure/Instrument.java
@@ -1,8 +1,6 @@
 package fr.polytech.kernel.structure;
 
 import fr.polytech.kernel.util.dictionnaries.MidiInstrument;
-import lombok.Getter;
 
-@Getter
 public record Instrument(String name, MidiInstrument midiInstrument) {
 }
