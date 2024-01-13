55ef128d36db0cc8b6094fac55c775c4f46167f2
IgorMelnykSI <ihor.melnyk@etu.unice.fr>
2024-01-13 11:22:49 +0100

    gitignore modif

--- kernel/src/main/java/fr/polytech/kernel/util/addon/PartitionGenerator.java
+++ kernel/src/main/java/fr/polytech/kernel/util/addon/PartitionGenerator.java
@@ -1,4 +1,13 @@
 package fr.polytech.kernel.util.addon;
 
+import fr.polytech.kernel.structure.Clip;
+
 public class PartitionGenerator {
+
+    public void createPartitionFromClip(Clip clip){
+
+
+
+    }
+
 }
