2d8433e31981d1886fdaf2028c2c6bdab937829a
IgorMelnykSI <ihor.melnyk@etu.unice.fr>
2024-01-13 12:27:55 +0100

    ...

--- kernel/src/main/java/fr/polytech/kernel/util/addon/PartitionGenerator.java
+++ kernel/src/main/java/fr/polytech/kernel/util/addon/PartitionGenerator.java
@@ -2,11 +2,28 @@ package fr.polytech.kernel.util.addon;
 
 import fr.polytech.kernel.structure.Clip;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class PartitionGenerator {
 
-    public void createPartitionFromClip(Clip clip){
+    public void createPartitionFromClip(Clip clip, String fileName){
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName + ".tex"))) {
+            writer.write("\\documentclass{article}\n");
+            writer.write("\\usepackage{musixtex}\n");
+            writer.write("\\begin{document}\n");
+            writer.write("\\begin{music}");
+            writer.newLine();
+            // Music starts here
 
 
+            writer.write("\\end{music}\n");
+            writer.write("\\end{document}");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
